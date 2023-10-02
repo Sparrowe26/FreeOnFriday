d674e73bdd671c534d266305f39f7e4ea90272da
Michael Steffler <michaelsteffler@icloud.com>
2023-10-02 11:52:03 -0400

    added code related to collision

--- Free On Friday/Assets/playerController.cs
+++ Free On Friday/Assets/playerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,11 +8,16 @@ public class playerController : MonoBehaviour
 {
     Vector2 movementInput;
     Rigidbody2D rb;
+    public float offset = 0.05f;
+    public float moveSpeed;
+    public ContactFilter2D movementFilter;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2d>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -24,7 +30,7 @@ public class playerController : MonoBehaviour
     {
         if(movementInput != Vector2.zero)
         {
-
+            rb.Cast(movementInput, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime * offset);
         }
     }
 
