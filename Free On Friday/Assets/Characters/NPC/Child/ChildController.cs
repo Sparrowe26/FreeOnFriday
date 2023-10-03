bdbce8c209316cfd76fc0038f124c2d191d6d3f7
Zukey0304 <92282300+Zukey0304@users.noreply.github.com>
2023-10-03 18:02:26 -0400

    started to add code for sightlines

---
+++ Free On Friday/Assets/Characters/NPC/Child/ChildController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildController : MonoBehaviour
+{
+    public float viewRadius;
+    public float viewAngle;
+
+    public Vector3 DirFromAngle(float angleDeg)
+    {
+        return new Vector2(Mathf.Sin(angleDeg * Mathf.Deg2Rad), Mathf.Cos(angleDeg * Mathf.Deg2Rad));
+    }
+}
