5116148428c53c2e676dbd8bf4857ddbeca3133a
Michael Steffler <michaelsteffler@icloud.com>
2023-10-15 14:11:24 -0400

    changed script collidable

--- Free On Friday/Assets/Characters/Player/collidable.cs
+++ Free On Friday/Assets/Characters/Player/collidable.cs
@@ -2,26 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class interactor : MonoBehaviour
+public class collidable : MonoBehaviour
 {
-    private Collider2D colliderBox;
-    private ContactFilter2D filter;
-    private List<Collider2D> collidedObj;
-
     // Start is called before the first frame update
+    private Collider2D colliderBox;
+    [SerializeField] private ContactFilter2D filter;
+    private List<Collider2D> collidedObj = new List<Collider2D>(1);
+
+
     void Start()
     {
         colliderBox = GetComponent<Collider2D>();
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        colliderBox.OverlapCollider(filter,collidedObj);
-        foreach(var o in collidedObj)
+        colliderBox.OverlapCollider(filter, collidedObj);
+        foreach (var o in collidedObj)
         {
             Debug.Log(o.name);
         }
     }
 }
+
