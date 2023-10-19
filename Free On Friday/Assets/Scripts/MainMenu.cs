173757091f90c918b067890bb868757c0ef0e718
Ryan Vollmer <rjv1635@rit.edu>
2023-10-19 02:22:34 -0400

    Added a main menu

---
+++ Free On Friday/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Load Game
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Quit Game
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
