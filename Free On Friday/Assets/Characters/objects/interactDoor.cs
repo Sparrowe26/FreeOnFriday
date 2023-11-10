320fb68121d1e3ddefd4358c2a617ee695f63458
Michael Steffler <michaelsteffler@icloud.com>
2023-11-10 13:14:28 -0500

    door shake

--- Free On Friday/Assets/Characters/objects/interactDoor.cs
+++ Free On Friday/Assets/Characters/objects/interactDoor.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Text;
 using UnityEngine;
+using static System.Net.Mime.MediaTypeNames;
 
 public class interactDoor : interactable
 {
@@ -14,6 +15,62 @@ public class interactDoor : interactable
     protected GameObject key;
 
     [SerializeField] public bool isOpen;
+    
+    private bool shaking = false;
+    private float timer;
+    private float shakeTime = .5f;
+   
+    private Vector3 originalPosition;
+
+    bool up = false;
+    int counter=0;
+
+    protected override void Update()
+    {
+        if (text != null)
+            text.SetActive(false);
+        colliderBox.OverlapCollider(filter, collidedObj);
+        foreach (var o in collidedObj)
+        {
+            OnCollide(o.gameObject);
+        }
+
+
+        if (shaking)
+        {
+            var yOffset = .05f;
+
+            if(up)
+            {
+                yOffset = -.01f;
+                //up = false;
+            }
+            else
+            {
+                 yOffset = .01f;
+                //up = true;
+            }
+
+            counter++;
+            if(counter>50)
+            {
+                counter = 0;
+                up = !up;
+            }
+
+
+            doorObject.gameObject.transform.position = originalPosition + new Vector3(0, yOffset, 0);
+            timer += Time.deltaTime;
+            if (timer > shakeTime)
+            {
+                shaking = false;
+                doorObject.gameObject.transform.position = originalPosition;
+                Invoke("interactDelay", .2f);
+            }
+        }
+    }
+
+
     protected override void onInteract()
     {
         
@@ -29,6 +86,15 @@ public class interactDoor : interactable
                     interacted = true;
                     isOpen = true;
                     Invoke("interactDelay", .2f);
+                }
+                else
+                {
+                    originalPosition = doorObject.gameObject.transform.position; 
+                    timer = 0;
+                    shaking = true;
+                
+                    interacted = true;
+                   
                 }
             }
             else
