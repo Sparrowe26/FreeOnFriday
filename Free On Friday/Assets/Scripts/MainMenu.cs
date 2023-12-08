5ec8a94d6e4ab38cb61b117ef148b9c39fcab31c
Ryan Vollmer <rjv1635@rit.edu>
2023-12-07 19:07:06 -0500

    MainMenu Script can get to the game over screen

--- Free On Friday/Assets/Scripts/MainMenu.cs
+++ Free On Friday/Assets/Scripts/MainMenu.cs
@@ -6,8 +6,9 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     //Load Game into the first level
-    public string nextSceneTitle = "Onboarding";
-    public string firstScene = "MainMenu";
+    private string nextSceneTitle = "Onboarding";
+    private string firstScene = "MainMenu";
+    private string gameOver = "GameOver";
     public void Play()
     {
         SceneManager.LoadScene(nextSceneTitle);
@@ -16,6 +17,11 @@ public class MainMenu : MonoBehaviour
     public void Return()
     {
         SceneManager.LoadScene(firstScene);
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(gameOver);
     }
 
     //Quit Game
